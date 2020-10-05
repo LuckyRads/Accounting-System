07b6eebdbc29ecd081cc03f7165d803c3638c965
Matas <matas.malickas@stud.vgtu.lt>
2020-10-05 16:36:32 +0300

    ADD: Export and import of all data.

--- src/main/java/com/lucky/accountingsystem/service/ExportService.java
+++ src/main/java/com/lucky/accountingsystem/service/ExportService.java
@@ -1,6 +1,5 @@
 package com.lucky.accountingsystem.service;
 
-import com.lucky.accountingsystem.model.Category;
 import com.lucky.accountingsystem.model.Company;
 import com.lucky.accountingsystem.model.Person;
 import com.lucky.accountingsystem.model.SubCategory;
@@ -24,13 +23,13 @@ public class ExportService {
 
             switch (choice) {
                 case "all":
-                    exportAllData();
+                    exportAllData(people, companies, categories);
                     break;
                 case "cat":
                     exportCategoryData(categories);
                     break;
                 case "usr":
-                    manageUserExports();
+                    manageUserExports(people, companies);
                     break;
                 case "back":
                     return;
@@ -38,8 +37,63 @@ public class ExportService {
         }
     }
 
-    public static void exportAllData() {
+    public static void manageUserExports(List<Person> people, List<Company> companies) {
+        String message[] = {"Choose action",
+                "per - export people data",
+                "com - export companies data",
+                "back - to go back up one menu level"};
 
+        String choice = "";
+        while (true) {
+            MessageService.showMessage(message);
+            choice = InputService.getInput();
+
+            switch (choice) {
+                case "per":
+                    exportPeopleData(people);
+                    break;
+                case "com":
+                    exportCompaniesData(companies);
+                    break;
+                case "back":
+                    return;
+            }
+        }
+    }
+
+    public static void exportAllData(List<Person> people, List<Company> companies, List<SubCategory> categories) {
+        MessageService.showMessage(new String[]{"Export data",
+                "To save all data in current directory, type default",
+                "To save elsewhere, please put in destination file path where to save the output file"});
+
+        String choice = InputService.getInput();
+
+        String file = choice.equals("default") ? "output.ser" : choice;
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            for (SubCategory subCategory : categories) {
+                objectOutputStream.writeObject(subCategory);
+            }
+
+            for (Person person : people) {
+                objectOutputStream.writeObject(person);
+            }
+
+            for (Company company : companies) {
+                objectOutputStream.writeObject(company);
+            }
+
+            fileOutputStream.close();
+            objectOutputStream.close();
+        } catch (IOException e) {
+            System.out.println("Error! Something went wrong while exporting data." +
+                    "Please check if the destination path is correct");
+            exportCategoryData(categories);
+            return;
+        }
     }
 
     public static void exportCategoryData(List<SubCategory> categories) {
@@ -69,8 +123,58 @@ public class ExportService {
         }
     }
 
-    public static void manageUserExports() {
+    public static void exportPeopleData(List<Person> people) {
+        MessageService.showMessage(new String[]{"Export data",
+                "To save all data in current directory, type default",
+                "To save elsewhere, please put in destination file path where to save the output file"});
 
+        String choice = InputService.getInput();
+
+        String file = choice.equals("default") ? "output.ser" : choice;
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            for (Person person : people) {
+                objectOutputStream.writeObject(person);
+            }
+
+            fileOutputStream.close();
+            objectOutputStream.close();
+        } catch (IOException e) {
+            System.out.println("Error! Something went wrong while exporting data." +
+                    "Please check if the destination path is correct");
+            exportPeopleData(people);
+            return;
+        }
+    }
+
+    public static void exportCompaniesData(List<Company> companies) {
+        MessageService.showMessage(new String[]{"Export data",
+                "To save all data in current directory, type default",
+                "To save elsewhere, please put in destination file path where to save the output file"});
+
+        String choice = InputService.getInput();
+
+        String file = choice.equals("default") ? "output.ser" : choice;
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            for (Company company : companies) {
+                objectOutputStream.writeObject(company);
+            }
+
+            fileOutputStream.close();
+            objectOutputStream.close();
+        } catch (IOException e) {
+            System.out.println("Error! Something went wrong while exporting data." +
+                    "Please check if the destination path is correct");
+            exportCompaniesData(companies);
+            return;
+        }
     }
 
 }
--- src/main/java/com/lucky/accountingsystem/service/ImportService.java
+++ src/main/java/com/lucky/accountingsystem/service/ImportService.java
@@ -23,13 +23,13 @@ public class ImportService {
 
             switch (choice) {
                 case "all":
-                    importAllData();
+                    importAllData(people, companies, categories);
                     break;
                 case "cat":
                     importCategoryData(categories);
                     break;
                 case "usr":
-                    manageUserImports();
+                    manageUserImports(people, companies);
                     break;
                 case "back":
                     return;
@@ -37,8 +37,56 @@ public class ImportService {
         }
     }
 
-    public static void importAllData() {
+    public static void manageUserImports(List<Person> people, List<Company> companies) {
+        String message[] = {"Choose action",
+                "per - import people data",
+                "com - import companies data",
+                "back - to go back up one menu level"};
 
+        String choice = "";
+        while (true) {
+            MessageService.showMessage(message);
+            choice = InputService.getInput();
+
+            switch (choice) {
+                case "per":
+                    importPeopleData(people);
+                    break;
+                case "com":
+                    importCompaniesData(companies);
+                    break;
+                case "back":
+                    return;
+            }
+        }
+    }
+
+    public static void importAllData(List<Person> people, List<Company> companies, List<SubCategory> categories) {
+        MessageService.showMessage(new String[]{"Import data",
+                "To import all data from current directory, type default",
+                "Please put in destination file path of the file which contains all serialized data"});
+
+        String choice = InputService.getInput();
+
+        String file = choice.equals("default") ? "output.ser" : choice;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            categories.add((SubCategory) objectInputStream.readObject());
+            people.add((Person) objectInputStream.readObject());
+            companies.add((Company) objectInputStream.readObject());
+
+            fileInputStream.close();
+            objectInputStream.close();
+        } catch (Exception e) {
+            System.out.println("Error! Something went wrong while importing data." +
+                    "Please check if the destination path is correct");
+            e.printStackTrace();
+            importCategoryData(categories);
+            return;
+        }
     }
 
     public static void importCategoryData(List<SubCategory> categories) {
@@ -67,8 +115,56 @@ public class ImportService {
         }
     }
 
-    public static void manageUserImports() {
+    public static void importPeopleData(List<Person> people) {
+        MessageService.showMessage(new String[]{"Import data",
+                "To import all data from current directory, type default",
+                "Please put in destination file path of the file which contains all serialized data"});
 
+        String choice = InputService.getInput();
+
+        String file = choice.equals("default") ? "output.ser" : choice;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            people.add((Person) objectInputStream.readObject());
+
+            fileInputStream.close();
+            objectInputStream.close();
+        } catch (Exception e) {
+            System.out.println("Error! Something went wrong while importing data." +
+                    "Please check if the destination path is correct");
+            e.printStackTrace();
+            importPeopleData(people);
+            return;
+        }
+    }
+
+    public static void importCompaniesData(List<Company> companies) {
+        MessageService.showMessage(new String[]{"Import data",
+                "To import all data from current directory, type default",
+                "Please put in destination file path of the file which contains all serialized data"});
+
+        String choice = InputService.getInput();
+
+        String file = choice.equals("default") ? "output.ser" : choice;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            companies.add((Company) objectInputStream.readObject());
+
+            fileInputStream.close();
+            objectInputStream.close();
+        } catch (Exception e) {
+            System.out.println("Error! Something went wrong while importing data." +
+                    "Please check if the destination path is correct");
+            e.printStackTrace();
+            importCompaniesData(companies);
+            return;
+        }
     }
 
 }
