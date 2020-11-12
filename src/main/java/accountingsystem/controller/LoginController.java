aa3317d46f777b659274af767b27df277843c133
Matas <matas.malickas@stud.vgtu.lt>
2020-11-12 22:18:57 +0200

    ADD: Login authentication.

--- src/main/java/accountingsystem/controller/LoginController.java
+++ src/main/java/accountingsystem/controller/LoginController.java
@@ -1,12 +1,20 @@
 package accountingsystem.controller;
 
+import accountingsystem.hibernate.model.Company;
+import accountingsystem.hibernate.model.Person;
+import accountingsystem.hibernate.util.CompanyUtil;
+import accountingsystem.hibernate.util.PersonUtil;
 import accountingsystem.service.ViewService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.IOException;
 
 public class LoginController {
@@ -21,14 +29,22 @@ public class LoginController {
     private Button registerCompanyBtn;
 
     @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("accountingsystem");
+    CompanyUtil companyUtil = new CompanyUtil(entityManagerFactory);
+    PersonUtil personUtil = new PersonUtil(entityManagerFactory);
+
+    @FXML
     public void login() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainMenu.fxml"));
-        Parent root = loader.load();
-
-        MainMenuController mainMenuController = loader.getController();
-        mainMenuController.loadSystemInfo();
-
-        ViewService.openView((Stage) loginBtn.getScene().getWindow(), root);
+        if (validateUser()) openMainMenu();
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Wrong credentials!");
+            alert.showAndWait();
+        }
     }
 
     @FXML
@@ -50,6 +66,30 @@ public class LoginController {
         registerCompanyController.populateResponsiblePeopleSelect();
 
         ViewService.openView((Stage) registerCompanyBtn.getScene().getWindow(), root);
+    }
+
+    private void openMainMenu() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainMenu.fxml"));
+        Parent root = loader.load();
+
+        MainMenuController mainMenuController = loader.getController();
+        mainMenuController.loadSystemInfo();
+
+        ViewService.openView((Stage) loginBtn.getScene().getWindow(), root);
+    }
+
+    private boolean validateUser() {
+        for (Person person : personUtil.getAllPeople()) {
+            if (person.getEmail().equals(emailField.getText()) && person.getPassword().equals(passwordField.getText())) {
+                return true;
+            }
+        }
+        for (Company company : companyUtil.getAllCompanies()) {
+            if (company.getEmail().equals(emailField.getText()) && company.getPassword().equals(passwordField.getText())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
