85054cdefb39201189d6b7b37848928953295ef0
Matas <luckyrads@gmail.com>
2020-11-23 19:42:43 +0200

    ADD: Edit accounting system.

--- src/main/java/accountingsystem/rest/controller/AccountingSystemController.java
+++ src/main/java/accountingsystem/rest/controller/AccountingSystemController.java
@@ -1,15 +1,17 @@
 package accountingsystem.rest.controller;
 
+import accountingsystem.hibernate.model.AccountingSystem;
 import accountingsystem.hibernate.util.AccountingSystemUtil;
 import accountingsystem.service.JSONSerializer;
+import com.google.gson.Gson;
 import org.json.JSONException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.util.Properties;
 
 @RestController
 public class AccountingSystemController {
@@ -21,6 +23,29 @@ public class AccountingSystemController {
     @ResponseStatus(value = HttpStatus.OK)
     public String getAccountingSystem() throws JSONException {
         return JSONSerializer.serializeObject(accountingSystemUtil.getAccountingSystem());
+    }
+
+    @PostMapping(value = "accounting-system")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String editAccountingSystem(@RequestBody String request) throws JSONException {
+        Gson parser = new Gson();
+        Properties data = parser.fromJson(request, Properties.class);
+
+        String company = (String) data.get("company");
+        String dateCreated = (String) data.get("dateCreated");
+        String version = (String) data.get("version");
+
+        LocalDate date = LocalDate.parse(dateCreated);
+
+        AccountingSystem accountingSystem = accountingSystemUtil.getAccountingSystem();
+
+        accountingSystem.setCompany(company);
+        accountingSystem.setDateCreated(date);
+        accountingSystem.setVersion(version);
+
+        accountingSystemUtil.edit(accountingSystem);
+
+        return "Success";
     }
 
 }
