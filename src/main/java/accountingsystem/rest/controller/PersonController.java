eeea0eab5c44cffce97891e3ab1b8b87f806b467
Matas <luckyrads@gmail.com>
2020-11-23 18:35:15 +0200

    CHN: Main application URL.

--- src/main/java/accountingsystem/rest/controller/PersonController.java
+++ src/main/java/accountingsystem/rest/controller/PersonController.java
@@ -2,14 +2,14 @@ package accountingsystem.rest.controller;
 
 import accountingsystem.hibernate.util.PersonUtil;
 import accountingsystem.service.JSONSerializer;
+import com.google.gson.Gson;
 import org.json.JSONException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Properties;
 
 @RestController
 public class PersonController {
@@ -21,6 +21,16 @@ public class PersonController {
     @ResponseStatus(value = HttpStatus.OK)
     public String getAllPeople() throws JSONException {
         return JSONSerializer.serializeArray(personUtil.getAllPeople());
+    }
+
+    @PostMapping(value = "person/create")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String createPerson(@RequestBody String request) throws JSONException {
+        Gson parser = new Gson();
+        Properties data = parser.fromJson(request, Properties.class);
+        System.out.println(data.get("email"));
+        System.out.println(data.get("password"));
+        return "Success";
     }
 
 }
