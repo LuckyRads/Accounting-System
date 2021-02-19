6aa2e37c594368c6ef4dab6597071f3ec17c3ff5
Matas <luckyrads@gmail.com>
2021-02-19 12:25:37 +0200

    ADD: Dummy login.

--- front-end/src/containers/Login.js
+++ front-end/src/containers/Login.js
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../styles/Login.css";
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
+  const goToHome = () => history.push("/");
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  function login(event) {
     event.preventDefault();
-    console.log(email);
+    // TODO: Do a call to the back-end server
+    goToHome();
   }
 
   return (
     <div className="Login">
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={login}>
         <Form.Group size="lg" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control
